b166d37815f21f6188b42f67335c130b282b2fdd
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-04-19 16:02:26 +0300

    Add template for index view in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the blog index</h1>")
+    return render(request, "blog/index.html")
 
 
 def about(request):

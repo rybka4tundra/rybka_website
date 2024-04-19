9592f73368888fc646f9f6021e072a0457927fb3
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-04-19 14:04:08 +0300

    Add about view for blog app

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello, world. You're at the blog index</h1>")
+
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')

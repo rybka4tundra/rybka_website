46ee693d6caedf9a54adedd2eca14f9f1f449d4c
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-05-02 14:15:07 +0300

    Add template that lists all Post to index.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, "blog/index.html", context)
 
 
 def about(request):

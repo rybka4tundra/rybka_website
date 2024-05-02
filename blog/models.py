ce808902c7f11281e007723cfdbfbe0fbe334f21
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-05-02 13:57:09 +0300

    Create Post model for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone  # Import timezone for DateTimeField
+from django.contrib.auth.models import User  # Import User model
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)  # If a user created the post is deleted, posts are deleted as well.ß
+
+    # __str__() method returns how the Post is printed
+    def __str__(self):
+        return self.title

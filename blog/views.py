23bc7f046f71c6b41153fbeef5a3dde82060ffd0
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-07-01 20:06:56 +0300

    Change index page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
+from datetime import datetime
 
 
 def index(request):
+    now = datetime.now()
+    current_date = now.date()
+    posts = Post.objects.all()
     context = {
-        'posts': Post.objects.all()
+        'current_date': current_date,
+        'posts': posts
     }
     return render(request, "blog/index.html", context)
 

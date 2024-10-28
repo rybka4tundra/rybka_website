1fa5e7aa621c64913d55e37c61e99a8c15f70405
Andrey Rybkin <rybkinandreymb@yandex.ru>
2024-10-28 17:22:09 +0300

    Add delete post view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("<str:name>", views.greet, name="greet"),
-    path("create_post/", views.create_post, name="create-post")
+    path("create_post/", views.create_post, name="create-post"),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+from users.context_processors import profile
 from users.models import Profile
 from .models import Post
 from .forms import PostCreateForm
@@ -45,3 +47,13 @@ def create_post(request):
             'form': form
         }
         return render(request, "blog/create_post.html", context)
+
+@login_required
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, 'Post deleted successfully!')
+        return redirect('user_profile', request.user.username)  # Redirect to the post list or another page
+
